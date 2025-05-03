5a0f968aeae506e324d3c0847005de7d0e1ac9ea
xiaocai2011 <xiaocai20110123@163.com>
2025-05-04 06:16:09 +0800

    0.2 版本 可保存部分不常修改的选项以及从谱面文件读取信息

--- main.py
+++ main.py
@@ -6,17 +6,19 @@
 """
 
 import sys
-# import json
+import json
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtCore import Qt
 import win32gui
 import win32con
+import re
 
-# 获取当前控制台窗口的句柄
-console_window = win32gui.GetForegroundWindow()
+if sys.argv[-1] != 'debug':
+    # 获取当前控制台窗口的句柄
+    console_window = win32gui.GetForegroundWindow()
 
-# 隐藏控制台窗口
-win32gui.ShowWindow(console_window, win32con.SW_HIDE)
+    # 隐藏控制台窗口
+    win32gui.ShowWindow(console_window, win32con.SW_HIDE)
 
 
 def generate_title(**kwargs):
@@ -55,7 +57,7 @@ def generate_intro(**kwargs):
         result += '评级：' + difficulty + '\n'
     if pp_val:
         result += f"PP值：{pp_val}\n\n{line}\n\n"
-    result += f'\n{line}\n\n'
+        result += f'\n{line}\n\n'
     if keyboard:
         result += "键盘：" + keyboard + "\n"
     if link:
@@ -85,30 +87,57 @@ class IntroGenerator(QtWidgets.QMainWindow):
         )
 
         central_widget = QtWidgets.QWidget()
-        layout = QtWidgets.QGridLayout()
-        layout.setSpacing(20)
+        self.layout = QtWidgets.QGridLayout()
+        self.layout.setSpacing(20)
         self.setMinimumSize(800, 600)
         self.setStyleSheet("background-color: white;")
 
         central_widget = QtWidgets.QWidget()
-        layout = QtWidgets.QVBoxLayout()
-        central_widget.setLayout(layout)
+        self.layout = QtWidgets.QVBoxLayout()
+        central_widget.setLayout(self.layout)
+
+        dialog = QtWidgets.QFileDialog(self)
+        dialog.setWindowTitle('选择谱面文件')
+        dialog.setNameFilter('*.adofai')
+        filePath, fileType = dialog.getOpenFileName()
+
+        with open(filePath, 'r') as f:
+            data = f.read()
+            name = data[data.find('"song": "') + 9 : data.find('\n', data.find('"song": "')) - 3]
+            artist = data[data.find('"artist": "') + 11: data.find('\n', data.find('"artist": "')) - 3]
+            suru = data[data.find('"author": "') + 11: data.find('\n', data.find('"author": "')) - 3]
+            desc = data[data.find('"levelDesc": "') + 14: data.find('\n', data.find('"levelDesc": "')) - 3]
+
+            name = re.sub('<color=.*?>|</color>|<size=.*?>|</size>|<b>|</b>', '', name).replace('\\"', '\"')
+            artist = re.sub('<color=.*?>|</color>|<size=.*?>|</size>|<b>|</b>', '', artist).replace('\\"', '\"')
+            suru = re.sub('<color=.*?>|</color>|<size=.*?>|</size>|<b>|</b>', '', suru).replace('\\"', '\"')
+            desc = re.sub('<color=.*?>|</color>|<size=.*?>|</size>|<b>|</b>', '', desc).replace('\\"', '\"')
 
         # 标题参数组
         title_group = QtWidgets.QGroupBox("标题参数")
         title_group.setFont(self.font)
         title_layout = QtWidgets.QFormLayout()
 
+        try:
+            with open('confing.json', 'r', encoding='utf-8') as f:
+                data = f.read()
+                self.line = json.loads(data)['line']
+                self.keyboard = json.loads(data)['keyboard']
+        except FileNotFoundError:
+            self.line = ''
+            self.keyboard = ''
+
         self.tag_input = QtWidgets.QLineEdit()
         self.tag_input.setFont(self.font)
         self.tag_input.setPlaceholderText("例如：完美无瑕")
-        self.tag_label = QtWidgets.QLabel("<p>标签</p>\n")
+        self.tag_label = QtWidgets.QLabel("<p>标签<font color='#FF0000'> * 必填</font></p>\n")
         self.tag_label.setFont(self.font)
         title_layout.addRow(self.tag_label, self.tag_input)
 
         self.name_input = QtWidgets.QLineEdit()
         self.name_input.setFont(self.font)
         self.name_input.setPlaceholderText("例如：Plum-MEGAMIX2")
+        self.name_input.setText(name)
         self.name_label = QtWidgets.QLabel("<p>谱子名字<font color='#FF0000'> * 必填</font></p>\n")
         self.name_label.setFont(self.font)
         title_layout.addRow(self.name_label, self.name_input)
@@ -121,7 +150,7 @@ class IntroGenerator(QtWidgets.QMainWindow):
         title_layout.addRow(self.judgment_label, self.judgment_input)
 
         title_group.setLayout(title_layout)
-        layout.addWidget(title_group)
+        self.layout.addWidget(title_group)
 
         # 简介参数组
         intro_group = QtWidgets.QGroupBox("简介内容")
@@ -131,14 +160,15 @@ class IntroGenerator(QtWidgets.QMainWindow):
         self.line_input = QtWidgets.QLineEdit()
         self.line_input.setFont(self.font)
         self.line_input.setPlaceholderText("例如：--------------------")
+        self.line_input.setText(self.line)
         self.line_label = QtWidgets.QLabel("<p>分隔符<font color='#FF0000'> * 必填</font></p>\n")
         self.line_label.setFont(self.font)
-        # self.line_input.changeEvent(self.saveChange())
         intro_layout.addRow(self.line_label, self.line_input)
 
         self.text_input = QtWidgets.QLineEdit()
         self.text_input.setFont(self.font)
         self.text_input.setPlaceholderText("写一些你想说的话")
+        self.text_input.setText(desc)
         self.text_label = QtWidgets.QLabel("<p>文本</p>\n")
         self.text_label.setFont(self.font)
         intro_layout.addRow(self.text_label, self.text_input)
@@ -146,6 +176,7 @@ class IntroGenerator(QtWidgets.QMainWindow):
         self.artist_input = QtWidgets.QLineEdit()
         self.artist_input.setFont(self.font)
         self.artist_input.setPlaceholderText("例如：Plum")
+        self.artist_input.setText(artist)
         self.artist_label = QtWidgets.QLabel("<p>曲师</p>\n")
         self.artist_label.setFont(self.font)
         intro_layout.addRow(self.artist_label, self.artist_input)
@@ -153,6 +184,7 @@ class IntroGenerator(QtWidgets.QMainWindow):
         self.suru_input = QtWidgets.QLineEdit()
         self.suru_input.setFont(self.font)
         self.suru_input.setPlaceholderText("例如：Irin")
+        self.suru_input.setText(suru)
         self.suru_label = QtWidgets.QLabel("<p>谱师</p>\n")
         self.suru_label.setFont(self.font)
         intro_layout.addRow(self.suru_label, self.suru_input)
@@ -180,6 +212,7 @@ class IntroGenerator(QtWidgets.QMainWindow):
 
         self.keyboard_input = QtWidgets.QLineEdit()
         self.keyboard_input.setFont(self.font)
+        self.keyboard_input.setText(self.keyboard)
         self.keyboard_label = QtWidgets.QLabel("<p>键盘型号</p>\n")
         self.keyboard_label.setFont(self.font)
         intro_layout.addRow(self.keyboard_label, self.keyboard_input)
@@ -191,7 +224,7 @@ class IntroGenerator(QtWidgets.QMainWindow):
         intro_layout.addRow(self.link_label, self.link_input)
 
         intro_group.setLayout(intro_layout)
-        layout.addWidget(intro_group)
+        self.layout.addWidget(intro_group)
 
         # 生成按钮
         generate_button = QtWidgets.QPushButton("生成并保存")
@@ -202,16 +235,16 @@ class IntroGenerator(QtWidgets.QMainWindow):
             "color:white; padding:5px; border-radius: 8px;"
         )
         generate_button.clicked.connect(self.generate_intro)
-        layout.addWidget(generate_button, alignment=QtCore.Qt.AlignRight)
+        self.layout.addWidget(generate_button, alignment=QtCore.Qt.AlignRight)
 
         self.preview = QtWidgets.QTextEdit()
         self.preview.setFont(self.font)
         self.preview.setReadOnly(True)
         self.preview.setPlaceholderText("点击上方按钮查看结果")
         self.preview.setStyleSheet("border:1px solid #D0D0D0; background:#F5F5F5;")
-        layout.addWidget(self.preview)
+        self.layout.addWidget(self.preview)
 
-        central_widget.setLayout(layout)
+        central_widget.setLayout(self.layout)
         self.setCentralWidget(central_widget)
         self.show()
 
@@ -247,6 +280,15 @@ class IntroGenerator(QtWidgets.QMainWindow):
         output_str = f"标题：\n{title_part}\n\n简介：\n{intro_content}"
 
         self.preview.setText(output_str)
+
+        with open('confing.json', 'w') as f:
+            conf = {
+                'line': self.line_input.text(),
+                'keyboard': self.keyboard_input.text()
+            }
+            f.write(json.dumps(conf))
+            f.close()
+
         with open("result.txt", "w") as file:
             file.write(output_str)
 
